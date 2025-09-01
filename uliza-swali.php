3eb30cb5a134fbbad13b5dbedc675cf580bc445d
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-09-01 10:51:08 +0300

    fixed experts bio

--- uliza-swali.php
+++ uliza-swali.php
@@ -273,6 +273,9 @@ if (!empty($searchQuery)) {
     <?php include 'includes/footer.php'; ?>
 
     <!-- Additional Scripts -->
+    <!-- Bootstrap 5 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- AOS Animation -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
     <script>
