6a7ca59a76f04b2e54e374c553a253288a833f2c
KWAYU MMARI <84182991+kwayummari@users.noreply.github.com>
2025-09-17 17:39:58 +0300

    fixed mobile

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -16,10 +16,10 @@ $currentUser = $auth->getCurrentUser();
     <title>Admin Dashboard</title>
 
     <!-- Favicon -->
-    <link rel="icon" type="image/png" sizes="32x32" href="<?= asset('images/logo/logo.png') ?>">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?= asset('images/logo/logo.png') ?>">
-    <link rel="apple-touch-icon" href="<?= asset('images/logo/logo.png') ?>">
-    <link rel="shortcut icon" href="<?= asset('images/logo/logo.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= asset('assets/images/logo/favicon.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= asset('assets/images/logo/favicon.png') ?>">
+    <link rel="apple-touch-icon" href="<?= asset('assets/images/logo/favicon.png') ?>">
+    <link rel="shortcut icon" href="<?= asset('assets/images/logo/favicon.png') ?>">
 
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
